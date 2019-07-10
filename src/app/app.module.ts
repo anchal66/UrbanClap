32feb551c426b996a2296d7b3d61bf812ef043ce
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2019-07-10 23:10:31 +0530

    Added components n bootstrap, ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignOutComponent } from './sign-out/sign-out.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SignInComponent,
+    SignOutComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
